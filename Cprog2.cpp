32fb92dc5f1fc37673d9d8c0fbbc87133167e5f5
afoote1024 <afoote1024@aol.com>
2017-03-30 08:43:10 -0400

    Add files via upload

--- Cprog2.cpp
+++ Cprog2.cpp
@@ -9,7 +9,7 @@ int main()
 	{
 		char fname[10], fname1[10];
 		char lname[10], lname1[10];
-		int age = 0, age1=0;
+		int age = 0, age1 = 0;
 
 		FILE* binfile;
 	
@@ -23,17 +23,17 @@ int main()
 		}
     printf("%s", "Enter First Name");
     scanf("%s",fname);
-	printf("%s", fname);
+	
 	 printf("%s", "Enter Last Name");
     scanf("%s",lname);
-	printf("%s", lname);
+	
 	 printf("%s", "Enter Age");
      scanf("%d",&age);
-	printf("%d", age);
+	
 	fwrite(fname, sizeof(fname),1,binfile);
 	fwrite(lname, sizeof(lname),1,binfile);
 	fwrite(&age, sizeof(age),1,binfile);
-
+	fclose(binfile);
 
 
 	binfile=fopen("binfile.bin", "rb");
@@ -46,9 +46,8 @@ int main()
 	fread(fname1, sizeof(fname1),1,binfile);
 	fread(lname1, sizeof(lname1),1,binfile);
 	fread(&age1, sizeof(age1),1,binfile);
-	printf("%s%s%d",fname1,lname1, age);
-	//, lname1, age1);
-	fflush(stdin);
+	printf("%s%s%d",fname1,lname1, age1);
+	
 
 	fclose(binfile);
 }
